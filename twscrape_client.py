07a91081d17f6f518d1b135c518b43f540b89d90
dubbishpsychic0000 <52055007+dubbishpsychic0000@users.noreply.github.com>
2025-06-01 19:26:33 +0100

    Update twscrape_client.py

--- twscrape_client.py
+++ twscrape_client.py
@@ -37,7 +37,8 @@ def setup_driver() -> webdriver.Chrome:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    
+    options.add_argument("--disable-gpu")
+
     # Comment out headless for debugging - same as working script approach
     options.add_argument("--headless")
     
